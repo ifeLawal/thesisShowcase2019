d5b7ada12d61237f06c2d254e2afcbd00c44affe
IfeoluwaLawal <il693@nyu.edu>
2020-05-20 22:35:59 -0400

    Updates

---
+++ precache-manifest.594a1ac3bbb93219b4c0a5834d9c2afd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b078ca747823c624bdddcb00af7929ce",
+    "url": "./static/media/StudentProfiles.b078ca74.csv"
+  },
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.9eb600ee.js"
+  },
+  {
+    "revision": "43c4782507e64a558ef0",
+    "url": "./static/js/main.43c47825.chunk.js"
+  },
+  {
+    "revision": "cb1742472bd179e918fd",
+    "url": "./static/js/2.cb174247.chunk.js"
+  },
+  {
+    "revision": "43c4782507e64a558ef0",
+    "url": "./static/css/main.8c0a240b.chunk.css"
+  },
+  {
+    "revision": "f8a7223208b997419098dde7ca2d9192",
+    "url": "./index.html"
+  }
+];
